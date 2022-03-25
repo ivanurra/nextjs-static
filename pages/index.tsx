9305ff27afd16cc6246ed7ef83ad225b8ed21dfb
Ivan Urra <ivan.urra.wd@gmail.com>
2022-03-25 07:57:17 +0100

    NextUI Installed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@nextui-org/react";
 import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
   return (
     <>
       <h1>Hello World</h1>
+      <Button>Testing</Button>
     </>
   );
 };
