04137cefe15cd2485ba7a79b43be070f3856e07f
Ivan Urra <ivan.urra.wd@gmail.com>
2022-04-10 12:15:03 +0200

    WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,31 @@
-import { Button } from "@nextui-org/react";
-import { NextPage } from "next";
+import { NextPage, GetStaticProps } from "next";
 import { Layout } from "../components/layouts";
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log(props);
   return (
     <Layout title="Pokemon's List">
-      <h1>Hello World</h1>
-      <Button>Testing</Button>
+      <ul>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+      </ul>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  console.log("Hello World");
+
+  return {
+    props: {},
+  };
 };
 
 export default HomePage;
