a5bf1bf93ad0cc0203db1d80007f4ca5fd316010
Ivan Urra <ivan.urra.wd@gmail.com>
2022-04-20 08:37:45 +0200

    WIP

---
+++ pages/pokemon/1.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from "../../components/layouts/layout";
+
+const PokemonPage = () => {
+  return (
+    <Layout title="Some pokemon">
+      <h1>Hello World</h1>
+    </Layout>
+  );
+};
+
+export default PokemonPage;
