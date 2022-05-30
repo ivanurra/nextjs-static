89d68b73a3fd2b6d7f8554c542d5c3ee6b436511
Ivan Urra <ivan.urra.wd@gmail.com>
2022-05-30 09:44:02 +0200

    WIP

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,9 +1,27 @@
+import { Container, Text, Image } from "@nextui-org/react";
 import { Layout } from "../../components/layouts/layout";
 
 const FavoritesPage = () => {
   return (
     <Layout title="Pokémons - Favorites">
-      <h1>Favorites</h1>
+      <Container
+        css={{
+          display: "flex",
+          flexDirection: "column",
+          height: "calc(100vh-100px)",
+          alignItems: "center",
+          justifyContent: "center",
+          alignSelf: "center",
+        }}
+      >
+        <Text>No favorites yet</Text>
+        <Image
+          src="/Anime-Pokemon-PNG-Image.png"
+          alt="Logo"
+          width={200}
+          height={200}
+        />
+      </Container>
     </Layout>
   );
 };
