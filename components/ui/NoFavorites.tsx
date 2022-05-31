75509ae764690469a7736b3e55b3bed92eecc231
Ivan Urra <ivan.urra.wd@gmail.com>
2022-05-31 12:47:07 +0200

    WIP

---
+++ components/ui/NoFavorites.tsx
@@ -0,0 +1,26 @@
+import { Container, Text, Image } from "@nextui-org/react";
+
+export const NoFavorites = () => {
+  return (
+    <div>
+      <Container
+        css={{
+          display: "flex",
+          flexDirection: "column",
+          height: "calc(100vh-100px)",
+          alignItems: "center",
+          justifyContent: "center",
+          alignSelf: "center",
+        }}
+      >
+        <Text>No favorites yet</Text>
+        <Image
+          src="/Anime-Pokemon-PNG-Image.png"
+          alt="Logo"
+          width={200}
+          height={200}
+        />
+      </Container>
+    </div>
+  );
+};
--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,27 +1,10 @@
-import { Container, Text, Image } from "@nextui-org/react";
 import { Layout } from "../../components/layouts/layout";
+import { NoFavorites } from "../../components/ui";
 
 const FavoritesPage = () => {
   return (
     <Layout title="Pokémons - Favorites">
-      <Container
-        css={{
-          display: "flex",
-          flexDirection: "column",
-          height: "calc(100vh-100px)",
-          alignItems: "center",
-          justifyContent: "center",
-          alignSelf: "center",
-        }}
-      >
-        <Text>No favorites yet</Text>
-        <Image
-          src="/Anime-Pokemon-PNG-Image.png"
-          alt="Logo"
-          width={200}
-          height={200}
-        />
-      </Container>
+      <NoFavorites />
     </Layout>
   );
 };
