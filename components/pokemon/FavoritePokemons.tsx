c3471ba09d7bdbeccacd073a72e37bd8accc7e0b
Ivan Urra <ivan.urra.wd@gmail.com>
2022-06-08 07:23:45 +0200

    WIP

--- components/pokemon/FavoritePokemons.tsx
+++ components/pokemon/FavoritePokemons.tsx
@@ -1,23 +1,16 @@
-import { Card, Grid } from "@nextui-org/react";
-import { FC } from 'react';
+import { FC } from "react";
+import { Grid } from "@nextui-org/react";
+import { FavoriteCardPokemon } from "./FavoriteCardPokemon";
 
 interface Props {
-    pokemons: number[]
+  pokemons: number[];
 }
 
-export const FavoritePokemons: FC<Props> = ({pokemons}) => {
+export const FavoritePokemons: FC<Props> = ({ pokemons }) => {
   return (
     <Grid.Container gap={2} direction="row" justify="flex-start">
       {pokemons.map((id) => (
-        <Grid xs={6} sm={3} md={2} xl={1} key={id}>
-          <Card hoverable clickable css={{ padding: 10 }}>
-            <Card.Image
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
-              width={"100%"}
-              height={"140px"}
-            />
-          </Card>
-        </Grid>
+        <FavoriteCardPokemon key={id} pokemonId={id} />
       ))}
     </Grid.Container>
   );
--- components/pokemon/index.ts
+++ components/pokemon/index.ts
@@ -1,2 +1,3 @@
 export * from "./PokemonCard";
 export * from "./FavoritePokemons";
+export * from "./FavoriteCardPokemon";
