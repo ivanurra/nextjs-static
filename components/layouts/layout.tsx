14cb150ff7a8785f356203863dadf941679fcd15
Ivan Urra <ivan.urra.wd@gmail.com>
2022-06-22 06:43:34 +0200

    WIP

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -12,11 +12,11 @@ export const Layout: FC<Props> = ({ children, title }) => {
       <Head>
         <title>{title || "Pokemon App"}</title>
         <meta name="author" content="Iván Urra" />
-        <meta
-          name="description"
-          content={`Information about pokemon ${title}`}
-        />
+        <meta name="description" content={`Info about pokemon ${title}`} />
         <meta name="keywords" content={`${title}, pokemon, pokedex `} />
+        <meta property="og:title" content={`Information about ${title} `} />
+        <meta property="og:description" content={`Page about ${title} `} />
+        <meta property="og:image" content="http://localhost:3000/banner.png" />
       </Head>
       <Navbar />
       <main style={{ padding: "0px 20px" }}>{children}</main>
